385af4f31b2b1d51d39ac35457cc48cc1cd9ef0d
Louis Jordan <louisjoecodes@gmail.com>
2025-09-15 13:50:04 +0100

    remove unused dependency

--- packages/client/src/BaseConversation.ts
+++ packages/client/src/BaseConversation.ts
@@ -8,7 +8,6 @@ import type {
 import type {
   AgentAudioEvent,
   AgentResponseEvent,
-  AgentResponseCorrectionEvent,
   ClientToolCallEvent,
   IncomingSocketEvent,
   InternalTentativeAgentResponseEvent,
