d75fdd2224eb413877ed3fb7f2fc4680400b00e4
Calvin Leung <calvin@elevenlabs.io>
2025-11-27 20:40:21 +0900

    [Agent Widget] fix dark theme visual (#383)
    * feat: add dark syntax highlighting and syntax theme selection to widget
    
    * feat: implement theme toggle and refactor markdown playground to mimic real widget better
    
    * fix: add border for dark mode accessibility
    
    * tune markdown to use doc darkmode
    
    * feat: add @ctrl/tinycolor dependency and integrate color detection for syntax theme for semi backward compact
    
    * chore: update ESLint configuration to ignore distribution files
    
    * refactor: remove shadow in textarea
    
    * remove shadow from buttons
    
    * cleanup shadow naming

--- packages/convai-widget-core/tailwind.config.js
+++ packages/convai-widget-core/tailwind.config.js
@@ -28,11 +28,8 @@ export default {
       md: "0 2px 24px 1px rgba(0, 0, 0, 0.16)",
       lg: "0 2px 40px 1px rgba(0, 0, 0, 0.12)",
       header: "0 0 1px 0 rgba(0, 0, 0, 0.40), 0 2px 2px 0 rgba(0, 0, 0, 0.04)",
-      "natural-xs":
-          "0 0px 0px 1px rgba(0, 0, 0, 0.06), 0 1px 1px -0.5px rgba(0, 0, 0, 0.06), 0 3px 3px -1.5px rgba(0, 0, 0, 0.06)",
-      "natural-sm":
-        "0 0px 0px 1px rgba(0, 0, 0, 0.06), 0 1px 1px -0.5px rgba(0, 0, 0, 0.06), 0 3px 3px -1.5px rgba(0, 0, 0, 0.06), 0 6px 6px -3px rgba(0, 0, 0, 0.06), 0 12px 12px -6px rgba(0, 0, 0, 0.02)",
-      "upper-blur": "0 -8px 16px -4px rgba(0, 0, 0, 0.1)",
+      "scroll-fade-top": "0 -8px 16px -4px rgba(0, 0, 0, 0.1)",
+      "scroll-fade-bottom": "0 8px 16px -4px rgba(0, 0, 0, 0.1)",
     },
     data: {
       shown: 'shown="true"',
