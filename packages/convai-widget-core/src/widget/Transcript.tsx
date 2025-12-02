197610b32b1f8c2091bd3c78727c5dfd0496c2a6
Calvin Leung <calvin@elevenlabs.io>
2025-12-02 13:00:18 +0900

    feat: rewrite useStickToBottom hook for more consistent scroll with ResizeObserver

--- packages/convai-widget-core/src/widget/Transcript.tsx
+++ packages/convai-widget-core/src/widget/Transcript.tsx
@@ -9,18 +9,26 @@ interface TranscriptProps {
 }
 
 export function Transcript({ scrollPinned, transcript }: TranscriptProps) {
-  const { scrollContainer, handleScroll, handleUserInteraction, firstRender } =
-    useStickToBottom({ scrollPinned, transcript });
+  const {
+    scrollContainer,
+    contentRef,
+    handleScroll,
+    handleWheel,
+    handleTouchStart,
+    handleTouchMove,
+    firstRender,
+  } = useStickToBottom({ scrollPinned });
 
   return (
     <div
       ref={scrollContainer}
       className="px-4 pt-20 pb-4 grow overflow-y-auto z-2"
       onScroll={handleScroll}
-      onWheel={handleUserInteraction}
-      onTouchMove={handleUserInteraction}
+      onWheel={handleWheel}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
     >
-      <div className="flex flex-col gap-6">
+      <div ref={contentRef} className="flex flex-col gap-6">
         {transcript.value.map((entry, index) => (
           <TranscriptMessage
             key={`${index}-${entry.conversationIndex}`}
