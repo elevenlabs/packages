a99845fe57522377b086eeb60a420762d933f4ee
harshv-arch <harsh.v@myambergroup.com>
2026-02-16 15:35:54 +0530

    Fixes for build (#3)
    * Not needed code
    
    * Initial commit of types
    
    * Build fix
    
    * Delete not needed files
    
    * Fixes
    
    * Final fixes
    
    ---------
    
    Co-authored-by: Harsh Agarwal <harsh.agarwal@elyments.com>

---
+++ packages/types/src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * @ambernexus/types
+ *
+ * TypeScript types for AmberNexus Agent API communication
+ */
+
+export * as Incoming from "../generated/types/incoming";
+
+export * as Outgoing from "../generated/types/outgoing";
+
+export * from "../generated/types/asyncapi-types";
+
+// SDK-specific shared types
+export * from "./types";
